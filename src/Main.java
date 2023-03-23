cd5c89d4a04419b13dee48f5cb8e852accb6912d
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-23 20:08:04 +0100

    Pizzas manuales

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Pizzas pizza = new Pizzas("integral",true,3,true,true,false,false,true,true);
+        Pizzas pizza1 = new Pizzas("Integral",true,3,true,true,false,false,true,true);
 
-        System.out.println(pizza.toString());
+        System.out.println(pizza1.toString());
+
+        Pizzas pizza2 = new Pizzas("Normal",false,3,true,true,false,false,false,true);
+
+        System.out.println(pizza2.toString());
+
+        Pizzas pizza3 = new Pizzas("Fina",false,3,true,true,true,true,true,false);
+
+        System.out.println(pizza3.toString());
 
     }
 }
