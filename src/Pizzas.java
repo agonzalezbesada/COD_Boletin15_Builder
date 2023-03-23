d162ea9426a4970d1ef3baaa1cae182b40cbab75
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-23 20:38:19 +0100

    Pizza *tters

--- src/Pizzas.java
+++ src/Pizzas.java
@@ -1,38 +1,149 @@
 public class Pizzas {
 
-    private String masa;
+    private int masa;
+    private int size;
     private boolean relleno;
-    private int tipo;
     private boolean salsa;
     private boolean cebolla;
     private boolean sinGluten;
     private boolean pina;
     private boolean champinones;
     private boolean jamon;
+    private int recogida;
 
-    Pizzas(String masa, boolean relleno, int tipo, boolean salsa, boolean cebolla, boolean sinGluten, boolean pina, boolean champinones, boolean jamon) {
+    // Tipo de masa
+    public static final int NORMAL= 1;
+    public static final int FINA= 1;
+    public static final int PAN = 2;
+
+    // Tamaño de pizza
+    public static final int PEQUENA = 0;
+    public static final int MEDIANA = 2;
+    public static final int GRANDE = 3;
+
+    // Recogida
+
+    public static final int TIENDA = 0;
+    public static final int PARALLEVAR = 1;
+
+    public Pizzas() {
+        this.masa = NORMAL;
+        this.size = MEDIANA;
+        this.relleno = false;
+        this.salsa = false;
+        this.cebolla = false;
+        this.sinGluten = false;
+        this.pina = false;
+        this.champinones = false;
+        this.jamon = true;
+        this.recogida = TIENDA;
+    }
+
+    Pizzas(int masa, int size, boolean relleno, boolean salsa, boolean cebolla, boolean sinGluten, boolean pina, boolean champinones, boolean jamon, int recogida) {
         this.masa = masa;
+        this.size = size;
         this.relleno = relleno;
-        this.tipo = tipo;
         this.salsa = salsa;
         this.cebolla = cebolla;
         this.sinGluten = sinGluten;
         this.pina = pina;
         this.champinones = champinones;
         this.jamon = jamon;
+        this.recogida = recogida;
+    }
+
+    public int getMasa() {
+        return masa;
+    }
+
+    public void setMasa(int masa) {
+        this.masa = masa;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean getRelleno() {
+        return relleno;
+    }
+
+    public void setRelleno(boolean relleno) {
+        this.relleno = relleno;
+    }
+
+    public boolean getSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(boolean salsa) {
+        this.salsa = salsa;
+    }
+
+    public boolean getCebolla() {
+        return cebolla;
+    }
+
+    public void setCebolla(boolean cebolla) {
+        this.cebolla = cebolla;
+    }
+
+    public boolean getSinGluten() {
+        return sinGluten;
+    }
+
+    public void setSinGluten(boolean sinGluten) {
+        this.sinGluten = sinGluten;
+    }
+
+    public boolean getPina() {
+        return pina;
+    }
+
+    public void setPina(boolean pina) {
+        this.pina = pina;
+    }
+
+    public boolean getChampinones() {
+        return champinones;
+    }
+
+    public void setChampinones(boolean champinones) {
+        this.champinones = champinones;
+    }
+
+    public boolean getJamon() {
+        return jamon;
+    }
+
+    public void setJamon(boolean jamon) {
+        this.jamon = jamon;
+    }
+
+    public int getRecogida() {
+        return recogida;
+    }
+
+    public void setRecogida(int recogida) {
+        this.recogida = recogida;
     }
 
     @Override
     public String toString() {
         return "Pizza: " +
                 "masa=" + masa +
+                ", size=" + size +
                 ", relleno=" + relleno +
-                ", tipo=" + tipo +
                 ", salsa=" + salsa +
                 ", cebolla=" + cebolla +
                 ", sinGluten=" + sinGluten +
                 ", pina=" + pina +
                 ", champinones=" + champinones +
-                ", jamon=" + jamon;
+                ", jamon=" + jamon +
+                ", tipo de recogida=" + recogida;
     }
 }
